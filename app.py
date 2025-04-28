5c58f5c54e4fd91020b62e54d2cb85c75509a1fe
Angela Alvarez <angelamariaalag@gmail.com>
2025-04-27 20:16:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ st.title("Mi Primera App ")
 
 # Subtítulo e imagen 1
 st.subheader("Primera Sección: Un Inicio Inspirador")
-st.image("naruto.jpg", caption="Esta es la primera imagen", use_column_width=True)
+st.image("naruto.jpg", caption="Esta es la primera imagen", use_container_width=True)
 
 # Subtítulo e imagen 2
 st.subheader("Segunda Sección: Explorando Nuevos Horizontes")
-st.image("broadway.jpg", caption="Aquí exploramos algo nuevo", use_column_width=True)
+st.image("broadway.jpg", caption="Aquí exploramos algo nuevo", use_container_width=True)
 
-# Subtítulo e imagen 3
-st.subheader("Tercera Sección: Reflexión Final")
-st.image("supernatural", caption="Una reflexión para el final", use_column_width=True)
+
