a8d79ebdf1dbb40a5f615a71b5aa8d96ba5bf08b
Angela Alvarez <angelamariaalag@gmail.com>
2025-04-27 20:27:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ import streamlit as st
 from PIL import Image
 
 # Título de la aplicación
-st.title("Mi Aplicación con Subtítulos e Imágenes")
+st.title("Mi Primera Aplicacion con las cosas que me gustan")
+st.write("Solo para poner de ejemplo de muchas de las cosas que me gusto hice esta aplicacion")
 
 # Primera sección con subtítulo e imagen
-st.subheader("Primera Sección: Un Inicio Inspirador")
+st.subheader("Primera Sección: El anime")
 image1 = Image.open('naruto.jpg')  # Cambia 'imagen1.png' por el nombre de tu archivo
-st.image(image1, caption="Esta es la primera imagen", width=600)
+st.image(image1, caption="Aqui puse una imagen de uno de mis animes favoritos", width=600)
 
 # Segunda sección con subtítulo e imagen
-st.subheader("Segunda Sección: Explorando Nuevos Horizontes")
+st.subheader("Segunda Sección: Los musicales")
 image2 = Image.open('broadway.jpg')  # Cambia 'imagen2.png' por el nombre de tu archivo
-st.image(image2, caption="Aquí exploramos algo nuevo", width=600)
+st.image(image2, caption="Amo los musicales especialmente los de broadway", width=600)
 
 
 
